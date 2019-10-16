88658c70ca63fe284d60644ce1e147018f7f6344
Fiyori02 <fiorinahe2016@gmail.com>
2019-10-16 17:34:00 +0200

    add commit

---
+++ part1/courseinfo/README.old.md
@@ -0,0 +1,3 @@
+# courseinfo
+
+Hei
---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// const indefine
+const Header = "Half stack application develompment";
+const Header = (props)=> {
+  return {
+    <h1>
+    { props,course }
+    <h1/>
+    ); 
+  }
+
+    App = () {
+       const part1 = "Fundamentals of React"
+       const exercise1 = 10
+       const part2 = "Using props to pass data"
+       const exercise2 = 7
+       const part3 = "State of a component"
+       const exercise3 = 14
+       return (
+         <div>
+           <Header courseName = {Course}/>
+        <part name= {         
+           <p>
+  {part1} {exercise1}
+  <p/>
+
+
+           </div>
+       )
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
