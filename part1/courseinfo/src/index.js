ca68920508d4040fb4d5d67335198077a639d9a5
Fiyori02 <fiorinahe2016@gmail.com>
2019-10-23 18:20:02 +0200

    13

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -38,16 +38,6 @@ const App = () => {
     name: 'State of a component',
     exercises: 14,
   };
-  /*
-  This is a comment ..... TODO: DELETE ME !
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-*/
 
   return (
     <div>
@@ -56,11 +46,11 @@ const App = () => {
         part1={part1.name}
         exercises1={part1.exercises}
         part2={part2.name}
-        exercises2={part2.exercises2}
+        exercises2={part2.exercises}
         part3={part3.name}
-        exercises3={part3.exercises3}
+        exercises3={part3.exercises}
       />
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
